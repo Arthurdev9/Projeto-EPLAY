c32916c7ecc0f7bae2fc54951806560406936213
Arthur Vinicius <arthurvinicius081@hotmail.com>
2025-08-22 10:24:59 -0300

    feat(products): criar seção de produtos e estilizar componentes

---
+++ src/components/ProductsList/index.tsx
@@ -0,0 +1,54 @@
+import Product from '../Product'
+
+import { List, Container } from './styles'
+
+export type Props = {
+  title: string
+  background: 'gray' | 'black'
+}
+
+const ProductsList = ({ background, title }: Props) => {
+  return (
+    <Container background={background}>
+      <div className="container">
+        <h2>{title}</h2>
+        <List>
+          <Product
+            category="Ação"
+            description="teste"
+            image="https://placehold.co/222x250"
+            infos={['-10%', 'R$ 150']}
+            system="Windows"
+            title="Nome do jogo"
+          />
+          <Product
+            category="Ação"
+            description="teste"
+            image="https://placehold.co/222x250"
+            infos={['-10%', 'R$ 150']}
+            system="Windows"
+            title="Nome do jogo"
+          />
+          <Product
+            category="Ação"
+            description="teste"
+            image="https://placehold.co/222x250"
+            infos={['-10%', 'R$ 150']}
+            system="Windows"
+            title="Nome do jogo"
+          />
+          <Product
+            category="Ação"
+            description="teste"
+            image="https://placehold.co/222x250"
+            infos={['-10%', 'R$ 150']}
+            system="Windows"
+            title="Nome do jogo"
+          />
+        </List>
+      </div>
+    </Container>
+  )
+}
+
+export default ProductsList

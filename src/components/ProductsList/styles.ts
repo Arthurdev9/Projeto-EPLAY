43b150cc72ba6d5bc159f094f7413a4665762446
Arthur Vinicius <arthurvinicius081@hotmail.com>
2025-09-03 10:23:04 -0300

    feat(api): substituir dados hard coded por requisição à API e atualizar componentes

--- src/components/ProductsList/styles.ts
+++ src/components/ProductsList/styles.ts
@@ -11,14 +11,14 @@ export const Container = styled.section<Omit<Props, 'title' | 'games'>>`
 
   ${Card} {
     background-color: ${(props) =>
-      props.background === 'black' ? cores.cinza : cores.preta};
+      props.background === 'black' ? cores.cinza : cores.preta} !important;
   }
 `
 
 export const List = styled.ul`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
-  column-gap: 24px;
+  gap: 24px;
   margin-top: 40px;
 `
 
